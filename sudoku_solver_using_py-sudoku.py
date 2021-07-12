280516ca0ce4583c9c4719f20e73e7fef6bc8723
Bhoopesh R K <64587796+bhoopeshrk@users.noreply.github.com>
2021-07-12 18:43:52 +0530

    Update sudoku_solver_using_py-sudoku.py

--- sudoku_solver_using_py-sudoku.py
+++ sudoku_solver_using_py-sudoku.py
@@ -1,4 +1,7 @@
+#importing sudoku package
 from sudoku import Sudoku
+
+#creating the unsolved sudoku puzzle for which the solution needed
 board = [[8, 0, 0,  6, 0, 0,  0, 0, 0],
          [0, 0, 0,  0, 7, 0,  3, 8, 0],
          [0, 0, 5,  0, 2, 0,  0, 0, 0],
@@ -11,7 +14,10 @@ board = [[8, 0, 0,  6, 0, 0,  0, 0, 0],
          [0, 0, 0,  0, 0, 0,  0, 0, 9],
          [0, 0, 0,  9, 0, 1,  0, 6, 0 ]]
 
+#defining the unsolved sudoku puzzle in the sudoku package
 puzzle = Sudoku(3, 3, board=board)
 
+#Displaying the unsolved sudoku puzzle
 puzzle.show_full()
+#Displaying the solution for the unsolved sudoku puzzle
 puzzle.solve().show_full()
